6ae64c72c6325363e5ded32371340925f5d5c82b
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-21 14:02:55 +0900

    Initialization of project. Basic UI for the app of showing different 3D models in the real world. Relationship between cells and index of choice for an array of type String, change of color for the selection of option.

--- Ikea/ViewController.swift
+++ Ikea/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import ARKit
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    let itemsArray : [String] = ["CUP", "VASE", "BOXING", "TABLE"]
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
 }
 
